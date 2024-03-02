8156826caed95976aa42dee4e87ffdbec925dc1d
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2024-03-02 19:02:46 +0530

    registration form

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,6 @@ export default function Home() {
   return (
     <Flex direction="column" w="100w">
       <Box>
-        {/* <Box bgGradient="linear(purple.600 0%, purple.400 35%, purple.300 45%, white 55%)"> */}
         <Ticket />
         <Box>
           <SachiEvent />
